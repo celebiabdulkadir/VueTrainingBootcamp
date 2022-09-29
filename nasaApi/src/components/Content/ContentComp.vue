cbb4e1ab0f3135c137852cb9fa05176c01e963bc
Abdulkadir Celebi <abdulkadircelebi.ac@gmail.com>
2022-09-29 11:02:12 +0300

    Minor changes

--- nasaApi/src/components/Content/ContentComp.vue
+++ nasaApi/src/components/Content/ContentComp.vue
@@ -1,17 +1,16 @@
 <script setup>
-// import NewsKeyword from '../NewsKeyword/NewsKeyword.vue';
 const props = defineProps(['content'])
 
 </script>
 
 <template>
-<div v-for="item in props.content" :key="item">
-  <div class="headlineTitle"  >{{item.title}}</div>
-  <div class = "description"  > {{item.description}}</div>
-  <img class="imageUrl"   :src="item.image_url"  alt="" >
-  <div class = "pubDate" > Publish Date : {{item.pubDate}}</div>
-  <a  class = "newsUrl"  :href="item.link"  target="_blank"> Complete Article</a>
-</div>
+  <div v-for="item in props.content" :key="item">
+    <div class="headlineTitle"  >{{item.title}}</div>
+    <div class = "description"  > {{item.description}}</div>
+    <img class="imageUrl"   :src="item.image_url"  alt="" >
+    <div class = "pubDate" > Publish Date : {{item.pubDate}}</div>
+    <a  class = "newsUrl"  :href="item.link"  target="_blank"> Complete Article</a>
+  </div>
 </template>
 
 <style scoped>
@@ -79,26 +78,4 @@ const props = defineProps(['content'])
   margin-top: 8;
   margin-bottom: 15px;
 }
-/* 
-.root {
-    position: relative;
-}
-.modal {
-    position: absolute;
-    top: 0;
-    left: 0;
-    background-color: black;
-    width: 100% ;
-    height: 100% ;
-    display: flex ;
-    justify-content: center;
-    align-items: center;
-    
-
-}
-.modal > div {
-    background-color: black;
-    padding: 50px ;
-    border-radius: 10px;
-} */
 </style>
